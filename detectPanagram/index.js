289c481db68b134cf3f21d3c790a3e7ec66e5d5f
Pavel Kukharenko <adrigatus@yandex.ru>
2021-11-29 20:45:02 +0700

    add detect panagram kata

---
+++ detectPanagram/index.js
@@ -0,0 +1,6 @@
+function isPangram(string){
+    return string.split('');
+}
+
+console.log(isPangram("The quick brown fox jumps over the lazy dog."));
+console.log(isPangram("This is not a pangram."));
