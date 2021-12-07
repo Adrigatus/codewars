b9e3c603294704ebcb6f9f580dde7bd96e5b2f50
Pavel Kukharenko <adrigatus@yandex.ru>
2021-12-07 19:52:55 +0700

    solve valid brases kata

--- validBrases/index.js
+++ validBrases/index.js
@@ -1,12 +1,21 @@
 function validBraces(braces){
-    let counter = 0;
-
-    for(let i = 0; i < braces.length; i++) {
-        if(braces[i] == '(') {
-            counter++;
-            
+    let matches = { '(':')', '{':'}', '[':']' };
+    let stack = [];
+    let currentChar;
+  
+    for (var i=0; i<braces.length; i++) {
+      currentChar = braces[i];
+  
+      if (matches[currentChar]) { 
+        stack.push(currentChar);
+      } else { 
+        if (currentChar !== matches[stack.pop()]) {
+          return false;
         }
+      }
     }
+  
+    return stack.length === 0; 
 }
 
 console.log(validBraces( "()" ));
