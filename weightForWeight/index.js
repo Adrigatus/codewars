d347fbc74a0dbe3b6826f4c4b0b10764ed95af6c
Pavel <adrigatus@yandex.ru>
2021-12-20 21:09:33 +0700

    solve weght to weght kata

--- weightForWeight/index.js
+++ weightForWeight/index.js
@@ -1,3 +1,7 @@
 function orderWeight(strng) {
-    // your code
+    const sum = x => x.split('').reduce((res, cur) => (res + +cur), 0);
+    return str.split(' ').sort((a, b) => {
+      const diff = sum(a) - sum(b);
+      return diff == 0 ? (a > b ? 1 : -1) : diff;
+    }).join(' ');
 }
